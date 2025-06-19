6ca11ab4411dba92bda302297f95aa436a3f087f
Austin Pauley <austin2118ace@gmail.com>
2025-06-19 07:36:50 -0400

    separate module to hold the df save handles

---
+++ dewan_utils/async_io/save_df.py
@@ -0,0 +1,68 @@
+import os
+import pandas as pd
+
+
+def save_df_as_excel(self, df_to_save: pd.DataFrame, file_path: os.PathLike, *args, **kwargs) -> None:
+    """
+    Private function that saves a Dataframe. This function is submitted to the ThreadPoolExecuter as a job
+    Parameters
+    ----------
+    df_to_save (Pandas.DataFrame):
+        Pandas dataframe the user wishes to save to disk
+    file_path (os.PathLike):
+        File path with extension pointing to the save directory
+
+    Returns
+    -------
+        None
+    """
+    try:
+        df_to_save.to_excel(file_path, *args, **kwargs)
+    except Exception:
+        self.logger.error("Unable to save %s", file_path)
+    else:
+        self.logger.info("Saved %s", file_path)
+
+
+def save_df_as_csv(self, df_to_save: pd.DataFrame, file_path: os.PathLike, *args, **kwargs) -> None:
+    """
+    Private function that saves a Dataframe. This function is submitted to the ThreadPoolExecuter as a job
+    Parameters
+    ----------
+    df_to_save (Pandas.DataFrame):
+        Pandas dataframe the user wishes to save to disk
+    file_path (os.PathLike):
+        File path with extension pointing to the save directory
+
+    Returns
+    -------
+        None
+    """
+    try:
+        df_to_save.to_csv(file_path, *args, **kwargs)
+    except Exception:
+        self.logger.error("Unable to save %s", file_path)
+    else:
+        self.logger.info("Saved %s", file_path)
+
+
+def save_df_as_pickle(self, df_to_save: pd.DataFrame, file_path: os.PathLike, *args, **kwargs) -> None:
+    """
+    Private function that saves a Dataframe. This function is submitted to the ThreadPoolExecuter as a job
+    Parameters
+    ----------
+    df_to_save (Pandas.DataFrame):
+        Pandas dataframe the user wishes to save to disk
+    file_path (os.PathLike):
+        File path with extension pointing to the save directory
+
+    Returns
+    -------
+        None
+    """
+    try:
+        df_to_save.to_pickle(file_path, *args, **kwargs)
+    except Exception:
+        self.logger.error("Unable to save %s", file_path)
+    else:
+        self.logger.info("Saved %s", file_path)
